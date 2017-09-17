8f1a0b32b8a649fc4e232a52c24cb846eef18692
ivan <ivanzhangsolutions@gmail.com>
2017-09-17 11:56:06 -0400

    remove concurrency cuz its weird

--- files/grepper.go
+++ files/grepper.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"sync"
 
 	/* Third party */
 	// imports as "cli", pinned to v1; cliv2 is going to be drastically
@@ -32,9 +31,7 @@ func Grep(c *cli.Context) {
 			Args = append(Args, arg)
 		}
 
-		wg := sync.WaitGroup{}
-		filepath.Walk(WALKPATH, GWrapper(&wg))
-		wg.Wait()
+		filepath.Walk(WALKPATH, GWalker)
 	} else {
 		for _, arg := range c.Args() {
 			res, _ := storage.Get(arg)
@@ -45,42 +42,36 @@ func Grep(c *cli.Context) {
 	}
 }
 
-func GWrapper(wg *sync.WaitGroup) func(path string, f os.FileInfo, err error) error {
-	return func(path string, f os.FileInfo, err error) error {
-		go func() {
-			wg.Add(1)
-			defer wg.Done()
-			if verb {
-				fmt.Printf("touched: %s\n", path)
-			}
-
-			// only try to open existing files
-			if _, err := os.Stat(path); !os.IsNotExist(err) && !f.IsDir() {
-				isImage, err := IsImage(path)
-				if err != nil {
-					log.Fatal(err)
-				}
-				if isImage {
-					// rather than indexing in sqlite db, compare results from OCR
-					// scan with Args string slice
-					res := ocr.Process(path)
-					found := false
-					for j := 0; j < len(res); j++ {
-						r := res[j]
-
-						for i := 0; i < len(Args); i++ {
-							if strings.Contains(strings.ToLower(r), strings.ToLower(Args[i])) {
-								found = true
-							}
-						}
-					}
-
-					if found {
-						fmt.Println(path)
-					}
-				}
-			}
-		}()
-		return nil
+func GWalker(path string, f os.FileInfo, err error) error {
+	if verb {
+		fmt.Printf("touched: %s\n", path)
 	}
+
+	// only try to open existing files
+	if _, err := os.Stat(path); !os.IsNotExist(err) && !f.IsDir() {
+		isImage, err := IsImage(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if isImage {
+			// rather than indexing in sqlite db, compare results from OCR
+			// scan with Args string slice
+			res := ocr.Process(path)
+			found := false
+			for j := 0; j < len(res); j++ {
+				r := res[j]
+
+				for i := 0; i < len(Args); i++ {
+					if strings.Contains(strings.ToLower(r), strings.ToLower(Args[i])) {
+						found = true
+					}
+				}
+			}
+
+			if found {
+				fmt.Println(path)
+			}
+		}
+	}
+	return nil
 }
