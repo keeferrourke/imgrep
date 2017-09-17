c8c1f1b279e011634d41626dfac15665c3f1b547
ivan <ivanzhangsolutions@gmail.com>
2017-09-16 23:50:58 -0400

    frontend

--- server/main.go
+++ server/main.go
@@ -7,8 +7,11 @@ import (
 	// imports as "cli", pinned to v1; cliv2 is going to be drastically
 	// different and pinning to v1 avoids issues with unstable API changes
 	"encoding/json"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	cli "gopkg.in/urfave/cli.v1"
 
@@ -19,17 +22,34 @@ import (
 
 var PORT string
 
+type ResultRow struct {
+	Filename string `json:"filename"`
+	Bytes    []byte `json:"bytes"`
+}
+
 func StartServer(c *cli.Context) {
 	r := mux.NewRouter()
 	r.HandleFunc("/imgrep/search", func(w http.ResponseWriter, r *http.Request) {
-		keyword := r.PostFormValue("keyword")
+		keyword := r.FormValue("keyword")
 		filenames, err := storage.Get(keyword)
 		if err != nil {
 			log.Fatalf(err.Error())
 		}
 
-		resp := map[string][]string{}
-		resp["filenames"] = filenames
+		results := []*ResultRow{}
+		for _, file := range filenames {
+			f, err := ioutil.ReadFile(file)
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+
+			results = append(results, &ResultRow{
+				Filename: file,
+				Bytes:    f,
+			})
+		}
+		resp := map[string][]*ResultRow{}
+		resp["files"] = results
 
 		w.Header().Set("Content-Type", "application/json")
 		if err := json.NewEncoder(w).Encode(resp); err != nil {
@@ -37,6 +57,23 @@ func StartServer(c *cli.Context) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		templates, err := template.ParseFiles("./index.html")
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	s := http.StripPrefix("/assets/", http.FileServer(http.Dir(os.Getenv("GOPATH")+"/src/github.com/keeferrourke/imgrep/assets")))
+	log.Println(os.Getenv("GOPATH") + "/src/github.com/keeferrourke/imgrep/assets")
+	r.PathPrefix("/assets/").Handler(s)
 
 	http.ListenAndServe(":"+PORT, r)
 }
